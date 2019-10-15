ccf3d09eb7abbcf9eeaae8fa830de0df558e662c
HanByul Jang <j.1star.0726@gmail.com>
2019-10-15 09:39:54 +0900

    [Add] dependencies
    add dependencies for frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+import axios from 'axios'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(Vuex);
+Vue.use(VueRouter);
+Vue.prototype.$http = axios;
 
 new Vue({
   vuetify,
   // delimiters: ['[[', ']]'],
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
