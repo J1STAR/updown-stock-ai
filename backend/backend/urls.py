323496d462dbb1f1bf6877ff37bf8925cb5dedf6
정재욱 <skstk465@gmail.com>
2019-10-17 13:44:32 +0900

    check change element

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,8 +18,6 @@ from django.urls import path
 from django.views.generic import TemplateView
 from django.conf.urls import url
 
-from testJ import views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='index.html')),
--- backend/testJ/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TestjConfig(AppConfig):
-    name = 'testJ'
