e3e8864becf432c1f851fc5f57d76ce953fef887
rhalswo1234 <rhalswo1234@naver.com>
2019-10-23 17:31:08 +0900

    종가 시작가 고가 저가 각각을 가지고 종가를 예측

--- backend/LSTM_practice.py
+++ backend/LSTM_practice.py
@@ -73,8 +73,6 @@ plt.legend(["end price", "start price", "high price", "low price"])
 # print(df)
 # plt.show()
 #
-train = df.loc[:240, ["종가", "시가", "고가", "저가", "거래량"]]
-test = df.loc[240, ["종가"]]
 # print(train)
 # print(test)
 sc = MinMaxScaler()
@@ -118,6 +116,8 @@ df = pd.read_csv('cansim-0800020-eng-6674700030567901031.csv', skiprows=6, skipf
 
 # print(df.head())
 
+print(type(df))
+
 #  Jan-1991와 같은 형태를 1991-01-31과 같이 변형
 #  월을 숫자로 바꿔주고 일을 MonthEnd() 를 사용해서 각 월의 마지막날이 되도록 세팅
 df['Adjustments'] = pd.to_datetime(df['Adjustments'])+MonthEnd()
@@ -150,11 +150,11 @@ print(len(train))
 print(train)
 
 # print(test)
-ax = train.plot()
-# print(ax)
-# ax2 = test.plot()
-test.plot(ax=ax)
-plt.legend(['train', 'test'])
+# ax = train.plot()
+# # print(ax)
+# # ax2 = test.plot()
+# test.plot(ax=ax)
+# plt.legend(['train', 'test'])
 
 # plt.show()
 
@@ -184,104 +184,104 @@ for s in range(1, 13):
 # print(train.index)
 # print(test.index)
 #
-# X_train = train_sc_df.dropna().drop('Scaled', axis=1)
-# Y_train = train_sc_df.dropna()[['Scaled']]
-#
-# X_test = test_sc_df.dropna().drop('Scaled', axis=1)
-# Y_test = test_sc_df.dropna()[['Scaled']]
-#
-# # print(X_train.head())
-# # print(Y_train.head())
-#
-# X_train = X_train.values
-# # print("X_train.values")
-# # print(X_train)
-# X_test = X_test.values
-# Y_train = Y_train.values
-# Y_test = Y_test.values
-# # print(X_train.shape)
-# # print(X_train)
-# # print(Y_train.shape)
-# # print(Y_train)
-#
-# X_train_t = X_train.reshape(X_train.shape[0], 12, 1)
-# X_test_t = X_test.reshape(X_test.shape[0], 12, 1)
-# print("최종 DATA")
-# # print(X_train_t.shape)
-# print(type(X_train_t))
-# print(X_train_t)
-# # print(Y_train)
+X_train = train_sc_df.dropna().drop('Scaled', axis=1)
+Y_train = train_sc_df.dropna()[['Scaled']]
+
+X_test = test_sc_df.dropna().drop('Scaled', axis=1)
+Y_test = test_sc_df.dropna()[['Scaled']]
+
+# print(X_train.head())
+# print(Y_train.head())
+
+X_train = X_train.values
+# print("X_train.values")
+# print(X_train)
+X_test = X_test.values
+Y_train = Y_train.values
+Y_test = Y_test.values
+# print(X_train.shape)
+# print(X_train)
+# print(Y_train.shape)
+# print(Y_train)
+
+X_train_t = X_train.reshape(X_train.shape[0], 12, 1)
+X_test_t = X_test.reshape(X_test.shape[0], 12, 1)
+print("최종 DATA")
+# print(X_train_t.shape)
+print(type(X_train_t))
+print(X_train_t)
+# print(Y_train)
 
 # LSTM 모델 만들기
 # Clear session
 # 현재의 TF graph를 버리고 새로 만든다. 예전 모델, 레이어와의 충돌을 피한다.
-# K.clear_session()
-# model = Sequential() # Sequential Model
-# model.add(LSTM(20, input_shape=(12, 1)))# (timestep, feature)
-# # model.add(Dense(20))
-# # model.add(LSTM(20))
-# model.add(Dense(1)) # output = 1
-# model.compile(loss='mean_squared_error', optimizer='adam')
+K.clear_session()
+model = Sequential() # Sequential Model
+model.add(LSTM(20, input_shape=(12, 1)))# (timestep, feature)
+# model.add(Dense(20))
+# model.add(LSTM(20))
+model.add(Dense(1)) # output = 1
+model.compile(loss='mean_squared_error', optimizer='adam')
+
+# loss를 모니터링해서 patience만큼 연속으로 loss률이 떨어지지 않으면 훈련을 멈춘다.
+early_stop = EarlyStopping(monitor='loss', patience=7, verbose=1)
+
+# history=model.fit(X_train_t, Y_train, epochs=100, batch_size=30, verbose=1, callbacks=[early_stop])
+
+history = model.fit(X_train_t, Y_train, epochs=1000, verbose=1, batch_size=28, validation_data=(X_test_t, Y_test), callbacks=[early_stop])
+
+# Y_pred = model.predict(X_test_t)
+
+training_loss = history.history["loss"]
+test_loss = history.history["val_loss"]
+
+print(type(training_loss))
 #
-# # loss를 모니터링해서 patience만큼 연속으로 loss률이 떨어지지 않으면 훈련을 멈춘다.
-# early_stop = EarlyStopping(monitor='loss', patience=8, verbose=1)
-#
-# # history=model.fit(X_train_t, Y_train, epochs=100, batch_size=30, verbose=1, callbacks=[early_stop])
-#
-# history = model.fit(X_train_t, Y_train, epochs=1000, verbose=1, batch_size=10, validation_data=(X_test_t, Y_test), callbacks=[early_stop])
-#
-# # Y_pred = model.predict(X_test_t)
-#
-# training_loss = history.history["loss"]
-# test_loss = history.history["val_loss"]
-#
-# print(type(training_loss))
-# #
-# epoch_count = range(1, len(training_loss)+1)
-# #
-# # plt.plot(epoch_count, training_loss, "r--")
-# # plt.plot(epoch_count, test_loss, "b-")
-# # plt.legend(["Training Loss", "Test loss"])
-# # plt.xlabel("Epoch")
-# # plt.ylabel("Loss")
-# # plt.show()
-#
-# plt.figure(2)
+epoch_count = range(1, len(training_loss)+1)
 #
 # plt.plot(epoch_count, training_loss, "r--")
-#
 # plt.plot(epoch_count, test_loss, "b-")
-#
-# plt.legend(["Training Loss", "Test Loss"])
-#
+# plt.legend(["Training Loss", "Test loss"])
 # plt.xlabel("Epoch")
-# plt.ylabel("Loss Score")
+# plt.ylabel("Loss")
+# plt.show()
+
+plt.figure(2)
+
+plt.plot(epoch_count, training_loss, "r--")
+
+plt.plot(epoch_count, test_loss, "b-")
+
+plt.legend(["Training Loss", "Test Loss"])
+
+plt.xlabel("Epoch")
+plt.ylabel("Loss Score")
+
+Y_pred = model.predict(X_test_t)
+
+
+plt.figure(3)
+
+count= range(1, len(Y_pred)+1)
+# print(Y_test)
+# print(Y_pred)
+plt.plot(count, Y_test, "r--")
+plt.plot(count, Y_pred, "b-")
+
+plt.legend(["Y_test", "Y_pred"])
+
+Y_pred = model.predict(X_test_t)
+
+# plt.figure(4)
 #
-# Y_pred = model.predict(X_test_t)
-#
-#
-# plt.figure(3)
-#
-# count= range(1, len(Y_pred)+1)
-# # print(Y_test)
-# # print(Y_pred)
 # plt.plot(count, Y_test, "r--")
 # plt.plot(count, Y_pred, "b-")
 #
 # plt.legend(["Y_test", "Y_pred"])
-#
-# Y_pred = model.predict(X_test_t)
-#
-# # plt.figure(4)
-# #
-# # plt.plot(count, Y_test, "r--")
-# # plt.plot(count, Y_pred, "b-")
-# #
-# # plt.legend(["Y_test", "Y_pred"])
-#
-#
-#
-# plt.show()
+
+
+
+plt.show()
 
 
 
