8a6adc077a1a7f08c9fd70ad4b8a99002293b35d
Byeonggill Choi <impeach94@naver.com>
2019-10-28 08:46:30 +0900

    lstm test

--- backend/Stock_predictByClose.py
+++ backend/Stock_predictByClose.py
@@ -5,9 +5,10 @@ import matplotlib.pyplot as plt
 
 import tensorflow as tf
 
-from pandas.tseries.offsets import  MonthEnd
-
+from pandas.tseries.offsets import MonthEnd
 from sklearn.preprocessing import MinMaxScaler
+from keras.callbacks import ModelCheckpoint
+from keras.models import load_model
 
 
 from keras.layers import LSTM
@@ -22,7 +23,7 @@ import requests
 tf.compat.v1.set_random_seed(777)
 
 if __name__ == '__main__':
-   res = requests.get("http://j1star.ddns.net:8000/stock/067630")
+   res = requests.get("http://j1star.ddns.net:8000/stock/crop/316140")
    data = res.json()
    stock_info_list = data['corp']['stock_info']
    # date open high low close volumn
@@ -121,7 +122,7 @@ K.clear_session()
 
 model = Sequential()  # Sequential Model
 
-model.add(LSTM(40, input_shape=(sequence_length, 1)))  # (timestep, feature)
+model.add(LSTM(50, input_shape=(sequence_length, 1)))  # (timestep, feature)
 model.add(Dropout(0.2))
 
 # model.add(LSTM(20))
@@ -130,11 +131,11 @@ model.add(Dense(1))  # output = 1
 model.compile(loss='mean_squared_error', optimizer='adam')
 
 # loss를 모니터링해서 patience만큼 연속으로 loss률이 떨어지지 않으면 훈련을 멈춘다.
-# early_stop = EarlyStopping(monitor='loss', patience=10, verbose=1)
+early_stop = [EarlyStopping(monitor='val_loss', patience=20, verbose=1), ModelCheckpoint(filepath='best_model_close', monitor='val_loss', save_best_only=True)]
 
 # history=model.fit(X_train_t, Y_train, epochs=100, batch_size=30, verbose=1, callbacks=[early_stop])
 
-history = model.fit(X_train_t, Y_train, epochs=50, verbose=2, batch_size=10, validation_data=(X_test_t, Y_test))
+history = model.fit(X_train_t, Y_train, epochs=1000, verbose=2, batch_size=50, validation_data=(X_test_t, Y_test), callbacks=early_stop)
 
 # Y_pred = model.predict(X_test_t)
 
@@ -165,6 +166,10 @@ plt.ylabel("Loss Score")
 
 Y_pred = model.predict(X_test_t)
 
+best_model = load_model('best_model_close')
+
+Y_pred_best = best_model.predict(X_test_t)
+
 
 plt.figure(3)
 
@@ -178,18 +183,23 @@ plt.legend(["Y_test", "Y_pred_by_close"])
 
 Y_pred = model.predict(X_test_t)
 
-# plt.figure(4)
+plt.figure(4)
 #
-# plt.plot(count, Y_test, "r--")
-# plt.plot(count, Y_pred, "b-")
+plt.plot(count, Y_test, "r--")
+plt.plot(count, Y_pred_best, "b-")
 #
-# plt.legend(["Y_test", "Y_pred"])
+plt.legend(["Y_test", "Y_pred_best"])
+
+
+print(Y_test)
+print(Y_pred)
 
 count = 0
+best_count = 0
 for val in range(1, len(Y_test)):
     test_val = Y_test[val]-Y_test[val-1]
     pred_val = Y_pred[val]-Y_test[val-1]
-
+    pred_best_val = Y_pred_best[val]-Y_test[val-1]
     if test_val > 0:
         test_val = 1
     else:
@@ -198,12 +208,20 @@ for val in range(1, len(Y_test)):
         pred_val = 1
     else:
         pred_val = -1
+    if pred_best_val > 0:
+        pred_best_val = 1
+    else:
+        pred_best_val = -1
+    if test_val == pred_best_val:
+        best_count+=1
 
     if test_val == pred_val:
         count+=1
 
 print("count = ", count)
 print("총 개수 = ", len(Y_test))
-print("정답률 : ", count/len(Y_test))
+print("모델 정답률 : ", count/len(Y_test))
+print("best_count = ", best_count)
+print("베스트 모델 정답률 : ", best_count/len(Y_test))
 
 plt.show()
