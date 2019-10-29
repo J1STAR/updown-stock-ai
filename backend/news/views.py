589972109ece2f26fef9727bd9c52b73f7668294
HanByul Jang <j.1star.0726@gmail.com>
2019-10-29 16:48:14 +0900

    [Fix] url encoding corporation name
    fix url encoding corporation name

--- backend/news/views.py
+++ backend/news/views.py
@@ -5,12 +5,13 @@ from rest_framework.response import Response
 from datetime import datetime
 from bs4 import BeautifulSoup
 import requests
+import urllib.parse
 
 
 # Create your views here.
 class NewsView(APIView):
     def get(self, request, corp=None):
-        news_data = requests.get("https://news.google.com/search?q={}&hl=ko&gl=KR&ceid=KR:ko".format(corp))
+        news_data = requests.get("https://news.google.com/search?q={}&hl=ko&gl=KR&ceid=KR:ko".format(urllib.parse.quote(corp)))
 
         soup = BeautifulSoup(news_data.text, 'html.parser')
         news_data_list = soup.select("c-wiz > div > div > div > div > main > c-wiz > div > div > div > article > h3 > a")
