7ac711f11ff46fee889c7a729aa9269f8278a6b0
HanByul Jang <j.1star.0726@gmail.com>
2019-11-01 14:54:21 +0900

    [Modify] views & urls
    modify views & urls

--- backend/stock/views.py
+++ backend/stock/views.py
@@ -78,7 +78,7 @@ class StockDetailView(APIView):
 
             response = serializer.data
 
-        return Response(None, status=status.HTTP_200_OK)
+        return Response(response, status=status.HTTP_200_OK)
 
 
 class StockBusinessView(APIView):
